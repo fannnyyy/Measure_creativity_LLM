7400d203bf482fd11092d95caa760233af0e937a
Fanny Badoules <fanny.badoules@student-cs.fr>
2026-03-18 13:22:28 +0100

    update download dataset

--- download_dataset.py
+++ download_dataset.py
@@ -3,10 +3,16 @@ from huggingface_hub import snapshot_download, login
 
 login(token="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")
 
-snapshot_download(repo_id="ministere-culture/comparia-conversations", repo_type="dataset", local_dir="./data/comparia-conversations")
+#snapshot_download(repo_id="ministere-culture/comparia-conversations", repo_type="dataset", local_dir="./data/comparia-conversations")
 
-snapshot_download(repo_id="ministere-culture/comparia-votes", repo_type="dataset", local_dir="./data/comparia-votes")
+#snapshot_download(repo_id="ministere-culture/comparia-votes", repo_type="dataset", local_dir="./data/comparia-votes")
 
-snapshot_download(repo_id="ministere-culture/comparia-reactions", repo_type="dataset", local_dir="./data/comparia-reactions")
+#snapshot_download(repo_id="ministere-culture/comparia-reactions", repo_type="dataset", local_dir="./data/comparia-reactions")
+
+snapshot_download(
+    repo_id="ministere-culture/comparia-reactions",
+    repo_type="dataset",
+    local_dir="./data/comparia-reactions"
+)
 
 print("Fini")
