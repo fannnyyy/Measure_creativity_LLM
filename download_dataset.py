cd06f6e907a4be402a810c8930b683fe7f7f6cbb
Raphaël Vignal <raphael.vignal@student-cs.fr>
2026-03-24 16:17:00 +0100

    add ex 1.1

---
+++ download_dataset.py
@@ -0,0 +1,10 @@
+from huggingface_hub import snapshot_download, login
+
+
+snapshot_download(repo_id="ministere-culture/comparia-conversations", repo_type="dataset", local_dir="./data/comparia-conversations")
+
+snapshot_download(repo_id="ministere-culture/comparia-votes", repo_type="dataset", local_dir="./data/comparia-votes")
+
+snapshot_download(repo_id="ministere-culture/comparia-reactions", repo_type="dataset", local_dir="./data/comparia-reactions")
+
+print("Fini")
